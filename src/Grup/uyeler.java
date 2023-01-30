c3018195f0963124c3ee2de50c41cd7121df110f
LENOVO <fkye_vuslat@hotmail.com>
2023-01-30 22:47:16 +0300

    task 2 yapıldı..

--- src/Grup/uyeler.java
+++ src/Grup/uyeler.java
@@ -3,5 +3,6 @@ package Grup;
 public class uyeler {
     public static void main(String[] args) {
         System.out.println("herkes toplandı");
+        System.out.println("üyeler toplanan");
     }
 }
